2768f042ff9034e1befa447107bde33196d9fcbb
frenchiejnr <frenchiejnr@gmail.com>
2020-12-21 21:16:27 +0000

    add: grid clear

--- index.js
+++ index.js
@@ -13,11 +13,24 @@ function hoverChange() {
 
     squares.forEach((square) => {
         square.addEventListener('mouseover', (e) =>  {
-            console.log(e);
             e.target.classList.add('filled');
         })
     })
 }
 
+function clearGrid() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.classList.remove('filled');
+    })
+}
+let clearButton = document.createElement('button');
+
+document.querySelector('.container').before(clearButton);
+clearButton.textContent = 'Clear';
+clearButton.addEventListener('click', () => {
+    clearGrid();
+}
+);
 createGrid(16,16);
 hoverChange();
